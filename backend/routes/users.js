295900a4f786bc13c1335eca23940dd62884abf9
RalfiSlask <nilssonnarki@gmail.com>
2024-02-15 07:09:15 +0100

    feat: setup all endpoints and checking if connection and the endpoints work

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('../lib/conn');
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/all', function (req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: 'error with connection' });
+    }
+  });
+  res.json({ login: 'all works' });
+});
+
+/**
+ * Login user
+ */
+router.post('/login', (req, res) => {
+  connection.connect((err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: 'error with connection' });
+    }
+  });
+
+  res.send('login works');
+});
+
+/**
+ * Create user
+ */
+router.post('/add', (req, res) => {
+  connection.connect((err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: 'error with connection' });
+    }
+  });
+
+  res.send('add works');
 });
 
 module.exports = router;
