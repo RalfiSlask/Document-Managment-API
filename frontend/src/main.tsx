b03e96414f40e40c09c795701d742f57aa5945e2
RalfiSlask <nilssonnarki@gmail.com>
2024-02-15 12:45:11 +0100

    feat: import and use the context providers

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,19 @@ import LoginScreen from './components/pages/login/LoginScreen.tsx';
 import DocumentsScreen from './components/pages/documents/DocumentsScreen.tsx';
 import './styles/index.scss';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import { LoginProvider } from './context/LoginContext.tsx';
+import { DocumentsProvider } from './context/DocumentsContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<LoginScreen />} />
-        <Route path="/documents" element={<DocumentsScreen />} />
+        <LoginProvider>
+          <DocumentsProvider>
+            <Route path="/" element={<LoginScreen />} />
+            <Route path="/documents" element={<DocumentsScreen />} />
+          </DocumentsProvider>
+        </LoginProvider>
       </Routes>
     </HashRouter>
   </React.StrictMode>
