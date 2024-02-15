bcc6533849bee34e578729dbb50b8c8a8e76e51a
RalfiSlask <nilssonnarki@gmail.com>
2024-02-15 07:40:50 +0100

    feat: finalize post request for adding a new user, also add encryption with bcrypt

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const connection = require('../lib/conn');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
 
 /**
  * Get all users
@@ -35,6 +37,18 @@ router.post('/login', (req, res) => {
       console.log(err);
       return res.status(500).json({ error: 'error with connection' });
     }
+
+    const { email, password } = req.body;
+
+    connection.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'error with connection' });
+      }
+
+      console.log(result);
+      res.json(result);
+    });
   });
 
   res.send('login works');
@@ -44,14 +58,46 @@ router.post('/login', (req, res) => {
  * Create user
  */
 router.post('/add', (req, res) => {
+  const { name, email, password } = req.body;
   connection.connect((err) => {
     if (err) {
       console.log(err);
       return res.status(500).json({ error: 'error with connection' });
     }
-  });
 
-  res.send('add works');
+    let query = 'SELECT * FROM users';
+
+    connection.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'error with connection' });
+      }
+
+      const users = result;
+      const doesUserExist = users.find((user) => user.email === email);
+      if (doesUserExist) {
+        console.log('user already exist');
+        return res.status(404).json({ error: 'user already exist' });
+      }
+
+      bcrypt.hash(password, saltRounds, function (err, hash) {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({ error: 'error crypting password' });
+        }
+        let query = `INSERT INTO users (name, email, password) VALUES ("${name}", "${email}", "${hash}")`;
+        connection.query(query, (err, result) => {
+          if (err) {
+            console.log(err);
+            return res.status(500).json({ error: 'error with connection' });
+          }
+
+          console.log(result);
+          res.json(result);
+        });
+      });
+    });
+  });
 });
 
 module.exports = router;
