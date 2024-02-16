b558bd9d660f3f370bf668e9faf62ba88f69b279
RalfiSlask <nilssonnarki@gmail.com>
2024-02-16 07:49:58 +0100

    feat: add some custom colors, will add more later

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBG: '#FFFFFF',
+        secondaryBG: '#F1F3F6',
+        buttonBG: '#FD7401',
+        inputBG: '#F1F3F6',
+      },
+    },
   },
   plugins: [],
 };
