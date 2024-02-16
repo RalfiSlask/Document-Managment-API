0750e2bb10dea57b5d06a558c89cb4d0e3dda40c
RalfiSlask <nilssonnarki@gmail.com>
2024-02-16 16:46:58 +0100

    feat: new types for new document input values, document object and sections open object

--- frontend/src/utils/types/types.ts
+++ frontend/src/utils/types/types.ts
@@ -8,3 +8,25 @@ export interface ILoginFormInputValues {
   email: string;
   password: string;
 }
+
+export interface INewDocumentFormInputValues {
+  title: string;
+  description: string;
+  content: string;
+}
+
+export interface IDocumentObjectType {
+  content: string;
+  createData: Date;
+  deleted: number;
+  description: string;
+  document_id: number;
+  title: string;
+  user_id: number;
+}
+
+export interface ISectionsOpenType {
+  start: boolean;
+  create: boolean;
+  wysiwyg: boolean;
+}
