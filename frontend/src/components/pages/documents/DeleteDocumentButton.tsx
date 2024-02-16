8b92dc5286609e85fd789bb851e6aee47ea10b65
RalfiSlask <nilssonnarki@gmail.com>
2024-02-16 16:45:57 +0100

    feat: add delete button component to each document container

---
+++ frontend/src/components/pages/documents/DeleteDocumentButton.tsx
@@ -0,0 +1,32 @@
+import { DocumentsContext } from '../../../context/DocumentsContext';
+import { useContext } from 'react';
+import { LoginContext } from '../../../context/LoginContext';
+
+type DocumentButtonPropsType = {
+  documentId: number;
+};
+
+const DeleteDocumentButton: React.FC<DocumentButtonPropsType> = ({ documentId }) => {
+  const documentsContext = useContext(DocumentsContext);
+  const loginContext = useContext(LoginContext);
+
+  if (!documentsContext || !loginContext) {
+    return;
+  }
+
+  const { deleteDocument } = documentsContext;
+  const { userId } = loginContext;
+
+  return (
+    <button
+      onClick={() => {
+        deleteDocument(Number(userId), documentId);
+      }}
+      className="button-primary"
+    >
+      Delete
+    </button>
+  );
+};
+
+export default DeleteDocumentButton;
