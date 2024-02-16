eeda2195c8c09c89cdedf3ebcc16871c4ccd567a
RalfiSlask <nilssonnarki@gmail.com>
2024-02-16 07:56:21 +0100

    feat: add login screen and login inputs

---
+++ frontend/src/components/pages/login/LoginInput.tsx
@@ -0,0 +1,40 @@
+import { useContext } from 'react';
+import { LoginContext } from '../../../context/LoginContext';
+import { ILoginFormInputValues } from '../../../utils/types/types';
+import { getStringWithcapitalizedFirstLetter } from '../../../utils/types/helperfunctions';
+
+type CreateAccountProps = {
+  text: string;
+  inputKey: keyof ILoginFormInputValues;
+  type: string;
+};
+
+const LoginInput: React.FC<CreateAccountProps> = ({ type, text, inputKey }) => {
+  const loginContext = useContext(LoginContext);
+
+  if (!loginContext) {
+    return;
+  }
+
+  const { loginInputValues, handleLoginInputOnChange } = loginContext;
+
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <div className="flex justify-between items-center">
+        <label htmlFor={text}>{getStringWithcapitalizedFirstLetter(text)}</label>
+        <p className="text-red-500 h-[30px]"></p>
+      </div>
+      <input
+        onInput={e => handleLoginInputOnChange(inputKey, e)}
+        type={type}
+        spellCheck="false"
+        id={text}
+        placeholder={text}
+        className="input"
+        value={loginInputValues[inputKey]}
+      />
+    </div>
+  );
+};
+
+export default LoginInput;
--- frontend/src/components/pages/login/LoginScreen.tsx
+++ frontend/src/components/pages/login/LoginScreen.tsx
@@ -1,9 +1,34 @@
-function LoginScreen() {
+import LoginForm from './LoginForm';
+import LoginNotRegistered from './LoginNotRegistered';
+import { useContext } from 'react';
+import { LoginContext } from '../../../context/LoginContext';
+import GoBackButton from './GoBackButton';
+import CreateAccountForm from './CreateAccountForm';
+
+const LoginScreen = () => {
+  const loginContext = useContext(LoginContext);
+
+  if (!loginContext) {
+    return;
+  }
+
+  const { createAccountOpen } = loginContext;
+
   return (
-    <>
-      <h1 className="text-orange-500">Test</h1>
-    </>
+    <main>
+      {createAccountOpen ? (
+        <section>
+          <GoBackButton />
+          <CreateAccountForm />
+        </section>
+      ) : (
+        <section>
+          <LoginForm />
+          <LoginNotRegistered />
+        </section>
+      )}
+    </main>
   );
-}
+};
 
 export default LoginScreen;
