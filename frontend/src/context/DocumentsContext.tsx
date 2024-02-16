d7482aa712a16a5e147e74298f3716c5f0e5f63d
RalfiSlask <nilssonnarki@gmail.com>
2024-02-16 10:14:59 +0100

    feat: add state for documents and fetch function for getting all documents for a specifric user

--- frontend/src/context/DocumentsContext.tsx
+++ frontend/src/context/DocumentsContext.tsx
@@ -1,15 +1,43 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useState } from 'react';
+import { IDocumentObjectType } from '../utils/types/types';
 
 export const DocumentsContext = createContext<undefined | IDocumentsTypes>(undefined);
 
-interface IDocumentsTypes {}
+interface IDocumentsTypes {
+  getUserSpecificDocuments: (userId: string) => void;
+}
 
 interface IDocumentType {
   children: ReactNode;
 }
 
 export const DocumentsProvider: React.FC<IDocumentType> = ({ children }) => {
-  const contextValue = {};
+  const [documents, setDocuments] = useState<IDocumentObjectType[] | null>(null);
+
+  const getUserSpecificDocuments = async (userId: string) => {
+    console.log(userId);
+    try {
+      const response = await fetch(`http://localhost:3000/api/documents/${userId}`);
+      if (!response.ok) {
+        return;
+      }
+      const jsonData = await response.json();
+      if (jsonData) {
+        console.log(jsonData);
+        setDocuments(jsonData);
+      }
+    } catch (err) {
+      console.log(err, 'could not get documents');
+    }
+  };
+
+  const contextValue = {
+    // states
+    documents: documents,
+    // setters
+    // functions
+    getUserSpecificDocuments: getUserSpecificDocuments,
+  };
 
   return <DocumentsContext.Provider value={contextValue}>{children}</DocumentsContext.Provider>;
 };
