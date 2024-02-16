e290a7c9df0e08e7df2dfd54e54dfa244dfc6d4c
RalfiSlask <nilssonnarki@gmail.com>
2024-02-16 10:15:26 +0100

    feat: add documents screen

--- frontend/src/components/pages/documents/DocumentsScreen.tsx
+++ frontend/src/components/pages/documents/DocumentsScreen.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import { useContext, useEffect } from 'react';
+import { DocumentsContext } from '../../../context/DocumentsContext';
+import Sidebar from './Sidebar';
+import Header from './Header';
+import { LoginContext } from '../../../context/LoginContext';
 
 const DocumentsScreen = () => {
-  return <div>DocumentsScreen</div>;
+  const documentsContext = useContext(DocumentsContext);
+  const loginContext = useContext(LoginContext);
+
+  if (!documentsContext || !loginContext) {
+    return;
+  }
+
+  const { userId } = loginContext;
+  const { getUserSpecificDocuments } = documentsContext;
+
+  useEffect(() => {
+    getUserSpecificDocuments(userId);
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <main>
+        <section></section>
+      </main>
+    </>
+  );
 };
 
 export default DocumentsScreen;
