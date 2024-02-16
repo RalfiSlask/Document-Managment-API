9c3eb0940063ea4a14c5266ba42eadb925273a0f
RalfiSlask <nilssonnarki@gmail.com>
2024-02-16 10:16:12 +0100

    feat: add userId state and setting it when logging in user and also if there are stored values in localstorage

--- frontend/src/context/LoginContext.tsx
+++ frontend/src/context/LoginContext.tsx
@@ -13,6 +13,7 @@ interface ILoginTypes {
   createAccountErrorMessage: string;
   createAccountOpen: boolean;
   formSubmitted: boolean;
+  userId: string;
   // setters
   setIsFormValid: React.Dispatch<
     React.SetStateAction<{
@@ -23,7 +24,7 @@ interface ILoginTypes {
   setFormSubmitted: React.Dispatch<React.SetStateAction<boolean>>;
   // functions
   handleResetOfForm: (type: string) => void;
-  handleLoginSubmit: (e: FormEvent<HTMLFormElement>) => Promise<void>;
+  handleLoginSubmit: (e: FormEvent<HTMLFormElement>, navigate: (path: string) => void) => Promise<void>;
   handleLoginInputOnChange: (inputKey: keyof ILoginFormInputValues, e: FormEvent<HTMLInputElement>) => void;
   setCreateAccountFormStateOnClick: (state: boolean) => void;
   handleCreateAccountInputOnChange: (
@@ -45,6 +46,8 @@ interface ILoginChildrenType {
   children: ReactNode;
 }
 
+const storedUser = localStorage.getItem('user');
+
 export const LoginProvider: React.FC<ILoginChildrenType> = ({ children }) => {
   const [loginInputValues, setLoginInputValues] = useState({ email: '', password: '' });
   const [createAccountInputValues, setCreateAccountInputValues] = useState({ name: '', email: '', password: '' });
@@ -54,23 +57,15 @@ export const LoginProvider: React.FC<ILoginChildrenType> = ({ children }) => {
   const [isFormValid, setIsFormValid] = useState({ email: false, name: false });
   const [createAccountOpen, setCreateAccountOpen] = useState(false);
   const [formSubmitted, setFormSubmitted] = useState(false);
+  const [userId, setUserId] = useState(storedUser ? JSON.parse(storedUser).id : '');
 
-  const handleResetOfForm = (type: string) => {
-    if (type === 'login') {
-      setLoginInputValues({ email: '', password: '' });
-      setLoginErrorMessage('');
-    } else if (type === 'create') {
-      setCreateAccountInputValues({ name: '', email: '', password: '' });
-      setCreateAccountErrorMessage('');
-    }
-  };
-
-  const setCreateAccountFormStateOnClick = (state: boolean) => {
-    state ? setCreateAccountOpen(true) : setCreateAccountOpen(false);
-    state ? handleResetOfForm('login') : handleResetOfForm('create');
-  };
-
-  const postLoginUser = async () => {
+  /**
+   * Posts form inputs for the user login to server
+   * If auth is correct set states for name and id and also store in localstorage
+   * @param navigate navigation function for navigating to documents dashboard
+   * @returns void
+   */
+  const postLoginUser = async (navigate: (path: string) => void) => {
     try {
       const response = await fetch('http://localhost:3000/api/users/login', {
         method: 'POST',
@@ -84,15 +79,26 @@ export const LoginProvider: React.FC<ILoginChildrenType> = ({ children }) => {
         return;
       }
       const jsonData = await response.json();
-      // must send back json object with id and name
       if (jsonData.id) {
         console.log(jsonData);
         localStorage.setItem('user', JSON.stringify({ id: jsonData.id, name: jsonData.name }));
         setLoginErrorMessage('');
         setUserName(jsonData.name);
+        setUserId(jsonData.id);
+        navigate('/documents');
       }
     } catch (err) {
       console.log(err, 'could not post user');
+    }
+  };
+
+  const handleResetOfForm = (type: string) => {
+    if (type === 'login') {
+      setLoginInputValues({ email: '', password: '' });
+      setLoginErrorMessage('');
+    } else if (type === 'create') {
+      setCreateAccountInputValues({ name: '', email: '', password: '' });
+      setCreateAccountErrorMessage('');
     }
   };
 
@@ -136,14 +142,24 @@ export const LoginProvider: React.FC<ILoginChildrenType> = ({ children }) => {
     }
   };
 
-  const handleLoginSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  const handleLoginSubmit = async (e: FormEvent<HTMLFormElement>, navigate: (path: string) => void) => {
     e.preventDefault();
     const { email, password } = loginInputValues;
     if (email.trim().length <= 0 || password.trim().length <= 0) {
       setLoginErrorMessage('you have to fill in inputs');
     } else {
-      await postLoginUser();
+      await postLoginUser(navigate);
     }
+  };
+
+  /**
+   * Function for changing if create account section is open or not
+   * Also resets form inputs when changing sections
+   * @param state boolean
+   */
+  const setCreateAccountFormStateOnClick = (state: boolean) => {
+    state ? setCreateAccountOpen(true) : setCreateAccountOpen(false);
+    state ? handleResetOfForm('login') : handleResetOfForm('create');
   };
 
   const handleLoginInputOnChange = (inputKey: keyof ILoginFormInputValues, e: FormEvent<HTMLInputElement>) => {
@@ -196,6 +212,7 @@ export const LoginProvider: React.FC<ILoginChildrenType> = ({ children }) => {
   const contextValue = {
     // states
     userName: userName,
+    userId: userId,
     createAccountOpen: createAccountOpen,
     loginInputValues: loginInputValues,
     createAccountInputValues: createAccountInputValues,
