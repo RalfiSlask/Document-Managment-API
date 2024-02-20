ddf82388e334d2958d1b60b9571f88d0cf8875fa
RalfiSlask <nilssonnarki@gmail.com>
2024-02-20 13:16:59 +0100

    feat: add deletemodal with functionality

---
+++ frontend/src/components/shared/DeleteModal.tsx
@@ -0,0 +1,28 @@
+import CancelButton from './CancelButton';
+import { useContext } from 'react';
+import { DocumentsContext } from '../../context/DocumentsContext';
+import WYSIWYGDeleteButton from '../pages/documents/wysiwyg/WYSIWYGDeleteButton';
+import DeleteDocumentButton from '../pages/documents/start/DeleteDocumentButton';
+
+const DeleteModal = () => {
+  const documentsContext = useContext(DocumentsContext);
+
+  if (!documentsContext) {
+    return;
+  }
+
+  const { sectionsOpen, documentId } = documentsContext;
+
+  return (
+    <div className="flex flex-col justify-between fixed z-20 left-1/2 -translate-x-1/2 top-60 bg-secondaryBG text-center rounded-md h-[200px] w-[400px] p-4">
+      <p className="text-lg">Are you sure you want to delete document?</p>
+      <div className="w-full flex items-center justify-center gap-4">
+        {sectionsOpen.wysiwyg && <WYSIWYGDeleteButton />}
+        {sectionsOpen.start && <DeleteDocumentButton documentId={documentId} />}
+        <CancelButton />
+      </div>
+    </div>
+  );
+};
+
+export default DeleteModal;
