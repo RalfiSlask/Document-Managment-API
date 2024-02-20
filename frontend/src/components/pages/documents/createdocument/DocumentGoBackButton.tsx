b79bcf0d765c69aa6c92bc12f2358e503455320b
RalfiSlask <nilssonnarki@gmail.com>
2024-02-20 13:23:27 +0100

    feat: add go back button in the create document section

---
+++ frontend/src/components/pages/documents/createdocument/DocumentGoBackButton.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { DocumentsContext } from '../../../../context/DocumentsContext';
+
+const DocumentGoBackButton = () => {
+  const documentsContext = useContext(DocumentsContext);
+
+  if (!documentsContext) {
+    return;
+  }
+
+  const { handleClickOnGoBack } = documentsContext;
+
+  return (
+    <button onClick={handleClickOnGoBack} className="button-primary absolute left-52 top-24">
+      Go Back
+    </button>
+  );
+};
+
+export default DocumentGoBackButton;
