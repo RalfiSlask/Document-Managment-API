045673980e157d648a17a75fedb55fe78fdd8df0
RalfiSlask <nilssonnarki@gmail.com>
2024-02-21 11:17:25 +0100

    feat: add go back button to startscreen

---
+++ frontend/src/components/pages/documents/wysiwyg/WYSIWYGGoBackButton.tsx
@@ -0,0 +1,28 @@
+import { useContext } from 'react';
+import { DocumentsContext } from '../../../../context/DocumentsContext';
+import { LoginContext } from '../../../../context/LoginContext';
+
+const WYSIWYGGoBackButton = () => {
+  const documentsContext = useContext(DocumentsContext);
+  const loginContext = useContext(LoginContext);
+
+  if (!documentsContext || !loginContext) {
+    return;
+  }
+
+  const { handleClickOnAbortWYSIWYG } = documentsContext;
+  const { userId } = loginContext;
+
+  return (
+    <button
+      onClick={() => {
+        handleClickOnAbortWYSIWYG(userId);
+      }}
+      className="absolute top-24 left-30 button-primary"
+    >
+      Go Back
+    </button>
+  );
+};
+
+export default WYSIWYGGoBackButton;
