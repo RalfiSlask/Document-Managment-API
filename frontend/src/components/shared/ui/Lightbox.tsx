c90c8aa4cc95ac7ea7a6859e3e08198af5a39ad0
RalfiSlask <nilssonnarki@gmail.com>
2024-02-21 07:49:07 +0100

    feat: made changes to z-index

--- frontend/src/components/shared/ui/Lightbox.tsx
+++ frontend/src/components/shared/ui/Lightbox.tsx
@@ -1,5 +1,5 @@
 const Lightbox = () => {
-  return <div className="bg-white opacity-25 fixed w-full h-full top-0 left-0 z-10"></div>;
+  return <div className="bg-white opacity-25 fixed w-full h-full top-0 left-0 z-40"></div>;
 };
 
 export default Lightbox;
