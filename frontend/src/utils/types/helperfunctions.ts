918e9d2db77326fde5cc37dd8813d06278b05987
RalfiSlask <nilssonnarki@gmail.com>
2024-02-22 12:25:06 +0100

    feat: added function for getting back labels based on password strength

--- frontend/src/utils/types/helperfunctions.ts
+++ frontend/src/utils/types/helperfunctions.ts
@@ -2,3 +2,10 @@ export const getStringWithcapitalizedFirstLetter = (text: string) => {
   const firstLetterUppercase = text.charAt(0).toUpperCase() + text.slice(1);
   return firstLetterUppercase;
 };
+
+export const getPasswordStrengthTitle = (strength: number) => {
+  if (strength > 6) return 'Strong';
+  if (strength > 3) return 'Medium';
+  if (strength > 0) return 'Weak';
+  return 'None';
+};
