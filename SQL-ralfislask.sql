1034e8cd6aaa322bdc6905b8de0d257c1d095c1a
RalfiSlask <nilssonnarki@gmail.com>
2024-02-23 19:18:49 +0100

    feat: add sql database

--- SQL-ralfislask.sql
+++ SQL-ralfislask.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 23, 2024 at 10:25 AM
+-- Generation Time: Feb 23, 2024 at 06:17 PM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -44,7 +44,9 @@ CREATE TABLE `documents` (
 INSERT INTO `documents` (`document_id`, `user_id`, `title`, `description`, `content`, `createDate`, `deleted`) VALUES
 (115, '2d0b1404-9f66-4ee4-94cf-aaeacced9d4c', 'New Document', 'New document desc', '<p><span style=\"font-size: 24pt; font-family: terminal, monaco, monospace; color: rgb(186, 55, 42);\">This is a text!</span></p>', '2024-02-23 10:21:36', 0),
 (116, '6a23ab20-d1c5-4665-b1b9-ea8758060790', 'New Document', 'New document desc', '<p><span style=\"font-size: 36pt; color: rgb(132, 63, 161); background-color: rgb(230, 126, 35);\">This is a text for you Kalle!</span></p>', '2024-02-23 10:22:37', 0),
-(117, '6a23ab20-d1c5-4665-b1b9-ea8758060790', 'Another document', 'This is another document desc', '<p><span style=\"font-size: 18pt;\">Why will you delete me, please save me someone!</span></p>', '2024-02-23 10:23:41', 1);
+(117, '6a23ab20-d1c5-4665-b1b9-ea8758060790', 'Another document', 'This is another document desc', '<p><span style=\"font-size: 18pt;\">Why will you delete me, please save me someone!</span></p>', '2024-02-23 10:23:41', 0),
+(118, '6a23ab20-d1c5-4665-b1b9-ea8758060790', 'Another document', 'Another document desc', '', '2024-02-23 14:43:01', 0),
+(119, 'ac19f9b6-8041-44bd-ac2d-8a87f371b4a2', 'test-title', 'test-description', 'test-content', '2024-02-23 16:44:51', 0);
 
 -- --------------------------------------------------------
 
@@ -69,7 +71,8 @@ INSERT INTO `users` (`id`, `uuid`, `name`, `email`, `createDate`, `password`) VA
 (19, 'a7035bdf-b990-4866-8d14-4916d354dfe8', 'test', 'test1@mail.com', '2024-02-23 10:20:41', '$2b$12$PWuZ0.fbU7vljxLoSzzy/uukqNwkD11fumTmimqcIRUNQzkzx9w6C'),
 (20, 'ac19f9b6-8041-44bd-ac2d-8a87f371b4a2', 'nisse', 'nisse@mail.com', '2024-02-23 10:21:01', '$2b$12$orAwXUIU6Yg270z3z0ylrOJfCcDJnRu6f/ouDYvbBsE0mWg7etnWu'),
 (21, '6a23ab20-d1c5-4665-b1b9-ea8758060790', 'kalle', 'kalle@mail.com', '2024-02-23 10:21:10', '$2b$12$CeW/KvUoL.hgUeAiP7nFoOGkQ3v0NDxdIKqwgAuNHY8VyOwTPWUzm'),
-(22, '2d0b1404-9f66-4ee4-94cf-aaeacced9d4c', 'matte', 'matte@mail.com', '2024-02-23 10:21:17', '$2b$12$QshBednmg5801PMYH8RNuup0VSCzKCzjHNiw4oumRxxEHKMXk36eO');
+(22, '2d0b1404-9f66-4ee4-94cf-aaeacced9d4c', 'matte', 'matte@mail.com', '2024-02-23 10:21:17', '$2b$12$QshBednmg5801PMYH8RNuup0VSCzKCzjHNiw4oumRxxEHKMXk36eO'),
+(23, '92f31e82-f0fb-44ed-8e7f-c40922ee45e7', 'nasse', 'nasse@mail.com', '2024-02-23 16:40:57', '$2b$12$RT5w9PeBLsZ.IMP9fuJJc.Nw2iFhgeqVHHx9EupLK2.wwQ1v4ytbe');
 
 --
 -- Indexes for dumped tables
@@ -97,13 +100,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `documents`
 --
 ALTER TABLE `documents`
-  MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
+  MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- Constraints for dumped tables
